6aa22f96a97c588d7dc4382816ebbc601237824f
paul-chavernac <chavernac.paul.lurcat@gmail.com>
2019-01-31 16:08:45 +0100

    ok

--- src/Admin/UserAdmin.php
+++ src/Admin/UserAdmin.php
@@ -2,24 +2,21 @@
 
 namespace App\Admin;
 
-use App\Entity\User;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\AdminBundle\Form\Type\ModelListType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 final class UserAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
-        $formMapper->add('id', TextType::class);
+
         $formMapper->add('username', TextType::class);
         $formMapper->add('password', TextType::class);
         $formMapper->add('email', TextType::class);
-        $CategFieldOptions = [];
-        $formMapper->add('roles', ModelListType::class, $CategFieldOptions);
+
 
     }
 
